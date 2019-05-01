699af4ebcf78a9b6be1ddc3dbf78d29f3828c492
dinesh sivanandam <sivanandamdinesh@gmail.com>
2019-05-01 22:33:44 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Increment increment = new Increment {id = 1};
+            List<Increment> increments = new List<Increment>();
+            increments.Add(new Increment {id = 1});
+            increments.Add(new Increment {id = 2});
+            Account account = new Account {accountid = 1, increments = increments};
+            List<Account> accounts = new List<Account>();
+            accounts.Add(account);
+
+            var increment1 = increments.Where(x => x.id == 1).ToList();
+            var plans = new List<Plan>
+            {
+                new Plan
+                {
+                    accounts = accounts,
+                    planid = 1
+                }
+            };
+
+            var s = plans.Where(x => x.planid == 1)
+                .Select(x => new
+                {
+                    ////plans = x,
+                    account = x.accounts
+                        .Where(y => y.accountid == 1)
+                        .Select(z => new
+                            {
+                                increments = z.increments.Where(a => a.id == 0)
+                            }
+
+                        )
+                }).ToList();
+        }
+    }
+
+    public class Plan
+    {
+        public List<Account> accounts { get; set; }
+        public int planid { get; set; }
+    }
+
+    public class Account
+    {
+        public int accountid { get; set; }
+        public List<Increment> increments { get; set; }
+    }
+
+    public class Increment
+    {
+        public  int id { get; set; }
+    }
+}
